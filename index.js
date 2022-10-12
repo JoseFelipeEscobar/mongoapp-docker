54da1ed321175889be633bbb66f8fadeb5ee0606
jose felipe escobar ballesta <jfejose10@gmail.com>
2022-10-12 14:23:39 -0500

    adding server and config files

---
+++ index.js
@@ -0,0 +1,28 @@
+/* 
+@author Jose felipe Escobar 
+
+*/
+const express = require("express");
+const mongoose=  require ("mongoose");
+
+const Animal = mongoose.model('Animal', new mongoose.Schema({
+  tipo: String,
+  estado: String,
+}))
+
+const app = express()
+
+mongoose.connect('mongodb://jose:password@monguito:27017/miapp?authSource=admin')
+
+app.get('/', async (_req, res) => {
+  console.log('listando... perritos...')
+  const animales = await Animal.find();
+  return res.send(animales)
+})
+app.get('/crear', async (_req, res) => {
+  console.log('creando...')
+  await Animal.create({ tipo: 'Perrito', estado: 'Feliz' })
+  return res.send('ok')
+})
+
+app.listen(3000, () => console.log('listening...'))
